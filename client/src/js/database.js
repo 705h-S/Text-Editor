9788808dcd76eaa51e15e8e1a7c645f667ab42bc
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-04 21:30:31 -0800

    attempt to push to heroku 3

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,22 +14,35 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const jateDB = await openDB("jate", 1);
-  const tx = jateDB.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
-  const request = store.put({ jate: content });
-  const result = await request;
-  console.log("Data saved to the database", result);
+  console.log("PUT to the database");
+    const jateDb = await openDB("jate", 1);
+    const tx = jateDb.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    const objects = await store.getAll();
+    const contenttoWrite = objects.length > 0 ? { content: content, id: objects[0].id } : { content: content };
+
+    const request = await store.put(contenttoWrite);
+
+    const result = await request;
+    console.log("🚀 - data saved to the database", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const jateDB = await openDB("jate", 1);
-  const tx = jateDB.transaction("jate", "readonly");
-  const store = tx.objectStore("jate");
-  const request = store.getAll();
-  const result = await request;
-  console.log(result);
+  console.log("GET from the database");
+    const jateDb = await openDB("jate", 1);
+    const tx = jateDb.transaction("jate", "readonly");
+    const store = tx.objectStore("jate");
+    const request = store.getAll();
+    const result = await request;
+    
+    if (result.length > 0) {
+        console.log("result.value", result);
+        console.log(result[0].content);
+        return result[0].content;
+    } else {
+        return null;
+    }
 };
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,15 +3,16 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPrompt = event;
+  window.deferredPrompt = event;
   butInstall.classList.toggle("hidden", false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
+  const promptEvent = window.deferredPrompt;
+
   if (!promptEvent) {
-    return;
+      return;
   }
   promptEvent.prompt();
   window.deferredPrompt = null;
@@ -20,5 +21,5 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+  window.deferredPrompt = null;
 });
